cbcd6e4195d3fd79eb93650484e4146c689690ca
mone7231 <mone.evilboy@gmail.com>
2025-02-18 11:36:59 +0800

    commit 15

---
+++ src/app/[locale]/contact/page.tsx
@@ -0,0 +1,20 @@
+import { ContactForm } from "@/src/components/ContactForm";
+import { IndexNavBar } from "@/src/components/IndexNavBar";
+//import {setRequestLocale} from 'next-intl/server';
+
+type Props = {
+    params: {locale: string};
+  };
+
+  
+export default function ContactPage(){
+      // Enable static rendering
+    //setRequestLocale(locale);
+
+    return (
+        <div>
+            <IndexNavBar/>
+            <ContactForm />
+        </div>
+    );
+}
