405b32006bb226a3c472dccad2104fb50c532193
mone7231 <mone.evilboy@gmail.com>
2025-02-18 11:28:39 +0800

    commit 14

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-//import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig  = {
+const nextConfig  : NextConfig = {
      reactStrictMode: true,
      distDir: "build",
-     basePath: "/md-test",
+     //basePath: "/md-test",
 };
  
 export default withNextIntl(nextConfig);
