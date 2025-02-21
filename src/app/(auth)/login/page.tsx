522acd235c030eabf2f87349b0083a06fc11629f
mone7231 <mone.evilboy@gmail.com>
2025-02-21 15:38:21 +0800

    update install package and components

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,46 @@
+import type { Metadata } from "next";
+import { signIn } from '@/lib/auth';
+import { Button } from '@/src/components/parts/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/src/components/parts/card';
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login Page",
+};
 
 export default function LoginPage(){
       // Enable static rendering
     //setRequestLocale(locale);
 
     return (
-        <div>
-           return Login
+        <div className="min-h-screen flex justify-center items-start md:items-center p-8">
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                  This demo uses GitHub for authentication.
+                </CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <form
+                  action={async () => {
+                    'use server';
+                    await signIn('github', {
+                      redirectTo: '/'
+                    });
+                  }}
+                  className="w-full"
+                >
+                  <Button className="w-full">Sign in with GitHub</Button>
+                </form>
+              </CardFooter>
+            </Card>
         </div>
     );
 }
