da8a8d97db50bbaaef5d1e3fe921ce0441f003b8
mone7231 <mone.evilboy@gmail.com>
2025-02-22 17:39:06 +0800

    update nav bar

---
+++ src/app/(dashboard)/user.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/src/components/parts/button';
+
+import Image from 'next/image';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/src/components/parts/dropdown-menu';
+import Link from 'next/link';
+
+export async function User() {
+  let user = null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full"
+        >
+          <Image
+            src={'/placeholder-user.jpg'}
+            width={36}
+            height={36}
+            alt="Avatar"
+            className="overflow-hidden rounded-full"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        {user ? (
+          <DropdownMenuItem>
+           
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem>
+            <Link href="/login">Sign In</Link>
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
